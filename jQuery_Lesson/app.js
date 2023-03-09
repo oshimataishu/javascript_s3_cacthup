e5963dc5330ec1e7206e932ad24e4f35c5400db5
Taishu <oshimataishu@Taishu-MacBook-Air.local>
2023-03-09 17:35:57 +0900

    [finish]chapter5

--- jQuery_Lesson/app.js
+++ jQuery_Lesson/app.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-  $('body').html('<h1>Hello jQuery!</h1>');
-})
+$(function(){
+  $('.box1').slideDown(2000, function(){
+    $('.box1').css({
+      'width': '200px',
+      'height': '100px',
+      'background-color': '#00f'
+    }).slideUp(2000);
+  });
+});
