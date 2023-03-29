38cfd7935fe760b43096dbf6c38b6fd9cd49d149
Taishu <oshimataishu@Taishu-MacBook-Air.local>
2023-03-29 21:18:47 +0900

    [finish]chapter7

--- chapter_4/app.js
+++ chapter_4/app.js
@@ -1,9 +1,1 @@
-$(function(){
-  $('.box1').slideDown(2000, function(){
-    $('.box1').css({
-      'width': '200px',
-      'height': '100px',
-      'background-color': '#00f'
-    }).slideUp(2000);
-  });
-});
+$('body').html(("<h1>Hello Jquery!!</h1>"));
